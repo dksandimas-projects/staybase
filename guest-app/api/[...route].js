93a312f72fda97d5fa6663c49dac8b963947a9b4
dksandimas-projects <dksandimas.projects@gmail.com>
2026-07-09 23:01:03 +0800

    fix: update white logo to use nav-bar-logo-white.png in configuration and compiled api

--- guest-app/api/[...route].js
+++ guest-app/api/[...route].js
@@ -187017,7 +187017,7 @@ var config = {
   },
   logos: {
     standard: "FINAL LOGO.png",
-    white: "FINAL LOGO-white.png",
+    white: "nav-bar-logo-white.png",
     navbar: "nav-bar-logo.png",
     icon: "ICON LOGO.png",
     wordmark: "TEXT LOGO.png"
